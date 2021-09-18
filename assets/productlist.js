3e9b8966579094be21ea562043620041c649a01e
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-09-18 19:44:21 -0300

    Added function to render the product list in productlist.html and listeners for buy buttons

--- assets/productlist.js
+++ assets/productlist.js
@@ -1,8 +1,6 @@
 // Ejercicio: Clase 14/09/2021 (2/3)
-// guardamos los anchors que tenga la clase "sold-out";
-const soldOut = document.querySelectorAll('.sold-out');
-// guardamos el mensaje que se va a mostrar;
-const notAvaliable = document.getElementById('not-avaliable');
+
+addSoldOutListener()
 
 function toggleDisplayTemporarily(node) {
   if (node.classList.contains('d-none')) {
@@ -11,12 +9,16 @@ function toggleDisplayTemporarily(node) {
       node.classList.replace('d-flex', 'd-none');
     }, 3000)
   }
-  return
 };
-// por cada uno de los anchors se le asigna un event listener que reacciona al 'click'
-soldOut.forEach(el => el.addEventListener('click', function() {
-  toggleDisplayTemporarily(notAvaliable)
-}));
+
+function addSoldOutListener() {
+  const soldOut = document.querySelectorAll('.sold-out');
+  const notAvaliable = document.getElementById('not-avaliable');
+
+  soldOut.forEach(el => el.addEventListener('click', function() {
+    toggleDisplayTemporarily(notAvaliable)
+  }));
+}
 
 
 // Ejercicio: Clase 14/09/2021 (3/3)
@@ -43,3 +45,43 @@ window.addEventListener('scroll', () => {
   if (isHalfPage()) return setDisplayFlex(goUp)
   return setDisplayNone(goUp)
 })
+
+
+
+// Ejercicio: Clase 16/09/2021
+// Representamos la tienda con Shop, los productos estan dentro de catalogo y este a su vez en Shop
+const shop = new Shop
+
+// Emulamos una llamada a una API para conseguir los datos y crear productos.
+shop.fetchProduct()
+
+renderProductsList()
+addCartListener()
+
+function renderProductsList() {
+  // Injecta todos los nft sin filtros
+  const allNfts = document.getElementById('all-nfts')
+  shop.catalogue.getAll().forEach(product => allNfts.innerHTML += productItem(product))
+
+  // Injecta en el DOM todos los productos filtrados por su categoria.
+  const mostValuableNfts = document.getElementById('most-valuable-nft')
+  shop.catalogue.getByCategory('most-valuable').forEach(product => mostValuableNfts.innerHTML += productItem(product))
+
+  const colorfulNfts = document.getElementById('colorful-nfts')
+  shop.catalogue.getByCategory('colorful').forEach(product => colorfulNfts.innerHTML += productItem(product))
+
+  const strangeNfts = document.getElementById('strange-nfts')
+  shop.catalogue.getByCategory('strange').forEach(product => strangeNfts.innerHTML += productItem(product))
+}
+
+function addCartListener() {
+  // Boton comprar - Agrega al carrito el producto.
+  const buyBtn = Array.from(document.getElementsByClassName('js-add-to-cart'))
+  buyBtn.forEach(btn => {
+    btn.addEventListener('click', function () {
+      const productToAdd = shop.getCatalogue().getById(this.dataset.productId)
+      shop.getCart().add(productToAdd)
+      return
+    })
+  })
+}
