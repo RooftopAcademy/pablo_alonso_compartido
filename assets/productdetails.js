93c0f4a56b33119c9158be2baf5604b5dd07064a
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-09-22 10:16:52 -0300

    Add handle error for users

--- assets/productdetails.js
+++ assets/productdetails.js
@@ -4,6 +4,7 @@ const shop = new Shop
 fetchProduct()
 
 async function fetchProduct() {
+  // const URL = 'https://wrongurl.com/' // To test the error message to the client
   const URL = 'https://my-json-server.typicode.com/Alonso-Pablo/api-nft/products'
   await fetchData(URL).then(data => {
     if (data === undefined) throw new Error('Fetch error')
@@ -13,6 +14,10 @@ async function fetchProduct() {
 }
 
 function renderCommentsList() {
-  const comments = shop.getCatalogue().getById('CdeG4by6YE826o0RegX1').comment
+  const product = shop.getCatalogue().getById('CdeG4by6YE826o0RegX1')
+  // Si no existe el producto se injecta un mensaje de error para el usuario al DOM.
+  if (!product) return injectSingleInDOM(product, document.getElementById('comments-list'), commentItem)
+  // Si existe el producto se injecta el/los comentarios al DOM.
+  const comments = product.comment
   return injectArrayInDOM(comments, document.getElementById('comments-list'), commentItem)
 }
