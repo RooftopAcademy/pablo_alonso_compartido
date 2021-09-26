d0b9ab56b3d913e4ca21ed94b43a5294cfd067e1
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-09-26 19:16:45 -0300

    Added Class Members for save registered users

---
+++ src/Members.ts
@@ -0,0 +1,13 @@
+import RegisteredUser from "./RegisteredUser";
+
+export default class Members {
+  private members: RegisteredUser[] = []
+
+  public getAll(): RegisteredUser[] {
+    return this.members
+  }
+
+  public add(member: RegisteredUser) {
+    this.members.push(member)
+  }
+}
