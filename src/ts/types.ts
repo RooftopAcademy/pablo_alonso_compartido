d87b1e13772cab54903d815143c63a452c16756d
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-09-26 19:22:42 -0300

    Added code to connect the users classes with the app and Changed the name of interfaces

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,17 +1,28 @@
-export interface DataProduct {
+export interface ProductData {
   id: string
   picture: string
   title: string
   author: string
   description: string
   price: number
-  comments: CommentProduct[]
+  comments: ProductComment[]
   category: string[]
   isAvaliable: boolean
 }
 
-export interface CommentProduct {
+export interface ProductComment {
   date: string
   username: string
   message: string
+}
+
+export interface UserData {
+  name: string
+  email: string
+  password: string
+}
+
+export interface LogData {
+  email: string
+  password: string
 }
