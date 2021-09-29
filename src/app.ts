707a34fb2475ffcc1d7d17e44da549bc4fa9501e
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-09-29 12:03:04 -0300

    Changed if if if if => switch

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,17 @@ import productDetails from './pages/productdetails'
 
 const PATH: string = window.location.pathname;
 
-(function app(path: string): void {
-  if (path === '/pablo_alonso_compartido/index.html') return index()
-  if (path === '/pablo_alonso_compartido/productlist.html') return productList()
-  if (path === '/pablo_alonso_compartido/productdetails.html') return productDetails()
-  return index()
+(function app(path: string) : void {
+  switch (path) {
+    case '/pablo_alonso_compartido/index.html':
+      index()
+      break
+    case '/pablo_alonso_compartido/productlist.html':
+      productList()
+      break
+    case '/pablo_alonso_compartido/productdetails.html':
+      productDetails()
+      break
+  }
 })(PATH)
+
