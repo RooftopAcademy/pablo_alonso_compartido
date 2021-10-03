eaa54dc28283d03c9496c73843cbaa04d90a4ff8
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-10-03 17:37:10 -0300

    Fixing methods and adding news methods

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -2,16 +2,20 @@ import {ProductInterface} from '../interfaces/types'
 import ProductFactory from '../factories/ProductFactory'
 
 class ProductRepository {
-    private items : ProductInterface[] = []
+  private items : ProductInterface[] = []
 
-    public constructor(data : []) {
-        this.items = data.map(item => ProductFactory.create(item))
-    }
+  public constructor(data : ProductInterface[]) {
+    this.items = data.map((item: ProductInterface) => ProductFactory.create(item))
+  }
 
 
-    public get() : ProductInterface[] {
-        return this.items
-    }
+  public get(): ProductInterface[] {
+    return this.items
+  }
+
+  public getById(id: string): (ProductInterface | undefined) {
+    return this.items.find((item: (ProductInterface)) => item.id == id)
+  }
 }
 
 export default ProductRepository
