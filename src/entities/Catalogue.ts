053031381d77778cca66dc42df31c23f2a3392fa
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-10-09 21:35:34 -0300

    Modified class Catalogue, now Catalogue extends from List

--- src/entities/Catalogue.ts
+++ src/entities/Catalogue.ts
@@ -1,62 +1,9 @@
 import { ProductInterface } from '../interfaces/types'
+import List from './List'
 
-interface SortProductInterface {
-  a: ProductInterface
-  b: ProductInterface
-  key: keyof ProductInterface
-}
-
-export default class Catalogue {
-  private products: ProductInterface[] = []
+export default class Catalogue extends List {
 
   public add(p: ProductInterface[]): void {
-    this.products = p
-  }
-
-  public get(): ProductInterface[] {
-    return this.products
-  }
-
-  // public sortByTitle(order?: 'desc' | 'asc'): ProductInterface[] {
-  //   return (order === 'desc')
-  //   ?[...this.products].sort((a,b) => this.compareBy({a, b, key: "title"})).reverse()
-  //   :[...this.products].sort((a,b) => this.compareBy({a, b, key: "title"}))
-  // }
-
-  // public sortByTitle(property?: keyof ProductInterface, order?: 'desc' | 'asc'): ProductInterface[] {
-  //   const prop = property || 'title'
-  //   const sorted = this.sortBy(prop)
-  //   return this.orderBy(sorted, order)
-  // }
-
-  public sortByTitle(order?: 'desc' | 'asc'): ProductInterface[] {
-    const sorted = this.sortBy('title')
-    return this.orderBy(sorted, order)
-  }
-
-  public sortByAuthor(order?: 'desc' | 'asc'): ProductInterface[] {
-    const sorted = this.sortBy('author')
-    return this.orderBy(sorted, order)
-  }
-  public sortByPrice(order?: 'desc' | 'asc'): ProductInterface[] {
-    const sorted = this.sortBy('price')
-    return this.orderBy(sorted, order)
-  }
-
-
-  private orderBy(sorted: ProductInterface[], order?: 'desc' | 'asc'): ProductInterface[] {
-    return (order==='desc')
-      ? sorted.reverse()
-      : sorted
-  }
-
-  private sortBy(key: keyof ProductInterface): ProductInterface[] {
-    return [...this.products].sort((a,b) => this.compareBy({a, b, key}))
-  }
-
-  private compareBy({a, b, key}: SortProductInterface): (1 | -1 | 0) {
-    if ((a)[key] > (b)[key]) return 1
-    if ((a)[key] < (b)[key]) return -1
-    return 0
+    this.items = p
   }
 }
