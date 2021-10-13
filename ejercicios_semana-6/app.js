da815b77629ac17295ae27b1c950cbc98241bb4d
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-10-13 02:48:20 -0300

    Added files, images, CSS files, bundle.js and favicon.svg. Modified html files to integrate styles, logic and images

--- ejercicios_semana-6/app.js
+++ ejercicios_semana-6/app.js
@@ -1,62 +1,53 @@
-const http = require('http')
 const fs = require('fs')
+const http = require('http')
 
-const notFound = fs.readFileSync('./ejercicios_semana-6/pages/notfound.html')
+const pagesPath = 'views/'
+const staticPath = 'public/'
+const localPath = './ejercicios_semana-6/'
 
 const server = http.createServer((req, res) => {
 
-  res.writeHead(200, {'Content-Type': 'text/html'})
+  if (req.url.includes('.')) {
+    if (req.url.includes('.css')) res.writeHead(200, {'Content-Type': 'text/css'})
+    if (req.url.includes('.img')) res.writeHead(200, {'Content-Type': 'image/png'})
+    if (req.url.includes('.ico')) res.writeHead(200, {'Content-Type': 'image/x-icon'})
+    if (req.url.includes('.svg')) res.writeHead(200, {'Content-Type': 'image/svg+xml'})
+    if (req.url.includes('.js')) res.writeHead(200, {'Content-Type': 'text/javascript'})
 
-  if (req.url === '/') {
-    fs.readFile('./ejercicios_semana-6/pages/index.html', null, (err, data) => {
-      if (err) {
-        res.writeHead(404)
-        res.write(notFound)
-      }
-
-      res.write(data)
-      res.end()
-      return
-    })
-  }
-
-  if (req.url === '/list') {
-    fs.readFile('./ejercicios_semana-6/pages/productlist.html', null, (err, data) => {
-      if (err) {
-        res.writeHead(404)
-        res.write(notFound)
-      }
-
-      res.write(data)
-      res.end()
-      return
-    })
-  }
-
-  if (req.url === '/details') {
-    fs.readFile('./ejercicios_semana-6/pages/productdetails.html', null, (err, data) => {
-      if (err) {
-        res.writeHead(404)
-        res.write(notFound)
-      }
-
-      res.write(data)
-      res.end()
-      return
-    })
-  }
-
-  if (req.url === '/notfound') {
-    res.write(notFound)
+    res.write(fs.readFileSync(localPath + staticPath + req.url))
     res.end()
     return
   }
+
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  const notFound = fs.readFileSync(localPath + 'views/notfound.html')
+
+  if (req.url === '/') {
+    const page = fs.readFileSync(localPath + pagesPath + 'index.html')
+    res.write(page)
+    res.end()
+    return
+  }
+
+  if (req.url === '/list') {
+    const page = fs.readFileSync(localPath + pagesPath + 'productlist.html')
+    res.write(page)
+    res.end()
+    return
+  }
+
+  if (req.url === '/details' || req.url.includes('/details?')) {
+    const page = fs.readFileSync(localPath + pagesPath + 'productdetails.html')
+    res.write(page)
+    res.end()
+    return
+  }
+
+  res.write(notFound)
+  res.end()
+  return
 })
 
 server.listen(3005)
-
-// server.on('connection', (socket) => {
-//   console.log('New connection...')
-// })
 
 console.log('Listening on port 3005...')
