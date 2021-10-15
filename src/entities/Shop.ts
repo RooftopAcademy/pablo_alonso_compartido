d9792269356d798b7ab0c50118ac0b92196e4b68
Alonso Pablo <pablo_nicolasalonso@yahoo.com>
2021-10-15 19:08:59 -0300

    Deleted unnecesaries files and methods

--- src/entities/Shop.ts
+++ src/entities/Shop.ts
@@ -2,25 +2,15 @@ import Cart from './Cart'
 import User from './User'
 import InvitedUser from './InvitedUser'
 import RegisteredUser from './RegisteredUser'
-import Members from './Members'
-
-import {
-  RegisteredUserInterface
-} from '../interfaces/types'
 
 export default class Shop {
 
   private cart: Cart = new Cart
-  private members: Members = new Members
   private user: (InvitedUser | RegisteredUser) = new InvitedUser
 
 
   public getCart(): Cart {
     return this.cart
-  }
-
-  public getMembers(): Members {
-    return this.members
   }
 
   public getUser(): User {
@@ -34,24 +24,6 @@ export default class Shop {
 
   public isLoged(): boolean {
     return (!(this.user instanceof InvitedUser)) ?true :false
-  }
-
-  public registerUser(data: RegisteredUserInterface): void {
-    const user: RegisteredUser = new RegisteredUser
-    user.name = data.name
-    user.email = data.email
-    user.password = data.password
-    this.members.add(user)
-  }
-
-  public saveMembers(): void {
-    if (localStorage.regdUsers) {
-      const regdUsers: RegisteredUser[] = this.members.getAll()
-      const prevData: RegisteredUser[] = JSON.parse(localStorage.regdUsers)
-
-      localStorage.setItem('regdUsers', JSON.stringify(regdUsers.concat(prevData)))
-    }
-    localStorage.setItem('regdUsers', JSON.stringify(this.members.getAll()))
   }
 
   public logOut(): void {
