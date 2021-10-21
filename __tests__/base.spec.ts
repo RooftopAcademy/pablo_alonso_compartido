12cb487a70c1889d00ebb5e859dd8adc05b292f5
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2021-10-21 01:30:36 -0300

    Deleted test unnecessary

--- __tests__/base.spec.ts
+++
@@ -1,8 +0,0 @@
-import { sampleFunction } from '../src'
-
-describe("Concat the same string twice", () => {
-	test("Check the sampleFunction function", () => {
-		expect(sampleFunction("hello")).toEqual("hellohello")
-	})
-})
-
